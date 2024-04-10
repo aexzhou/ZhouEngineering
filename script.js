f41b8fe8b3669653b04f4870460b70b843caeef8
Alex Zhou <cosr330@gmail.com>
2024-04-09 23:41:30 -0700

    adsasdas

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header');
 
-    window.onscroll = function() {
-        if (window.pageYOffset > 100) {
-            header.classList.add("small-header");
-        } else {
-            header.classList.remove("small-header");
-        }
-    };
+    // window.onscroll = function() {
+    //     if (window.pageYOffset > 100) {
+    //         header.classList.add("small-header");
+    //     } else {
+    //         header.classList.remove("small-header");
+    //     }
+    // };
+    window.onscroll = function() {scrollFunction()};
 
     header.onclick = function() {
         window.scrollTo({top: 0, behavior: 'smooth'});
     };
 });
+
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementsByClassName("header").style.fontSize = "30px";
+
+    } else {
+      document.getElementsByClassName("header").style.fontSize = "90px";
+    }
+}
