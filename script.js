940a2cc16c50850e3923f7cdc2f559c4d8964ecb
aexzhou <126853855+aexzhou@users.noreply.github.com>
2024-04-18 04:02:47 -0700

    asasa

--- script.js
+++ script.js
@@ -1,27 +1,93 @@
+function adjustName() {
+    var name = document.getElementById('name');
+    var windowWidth = window.innerWidth;
+    const threshold1 = 1400;
+    const threshold2 = 1300;
+
+    if(windowWidth <= threshold1 && windowWidth >= threshold2){
+        name.style.opacity = (windowWidth - threshold2)/(threshold1-threshold2);
+    }else if (windowWidth < threshold2) {
+
+        name.style.opacity = 0;
+    }else{ 
+        name.style.opacity = 1;
+    }
+}
+
+function throttle(func, limit) { //improve performance 
+    let inThrottle;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    }
+}
+
+function scrollFunction() {
+    var windowWidth = window.innerWidth;
+    var navElements = document.getElementsByClassName("navigation");
+    const nav = navElements[0]; 
+    const responsive = 768;
+
+    if(windowWidth <= responsive){
+        nav.style.display = 'none';
+    }else{
+        if (navElements.length > 0) {  // handle 0 elemnt error
+        
+            const buttons = nav.getElementsByTagName('a');
+    
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                nav.style.padding = "6px 20px";
+                nav.style.fontSize = "16px"; 
+                nav.style.position = 'fixed'; 
+                nav.style.top = '0';
+                nav.style.width = '100%'; 
+                nav.style.boxSizing = 'border-box';
+    
+                for (let i = 0; i < buttons.length; i++) {
+                    buttons[i].style.letterSpacing = '3px'; 
+                }
+    
+            } else {
+                nav.style.padding = "16px 20px"; //reset defaults
+                nav.style.fontSize = "16px";  
+                nav.style.position = 'relative'; 
+                nav.style.display = 'flex';
+                nav.style.boxSizing = 'border-box';
+    
+                for (let i = 0; i < buttons.length; i++) {
+                    buttons[i].style.letterSpacing = 'normal';
+                }
+            }
+        }
+    } 
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('.header');
+    
+    scrollFunction();
+    adjustName();
 
-    // window.onscroll = function() {
-    //     if (window.pageYOffset > 100) {
-    //         header.classList.add("small-header");
-    //     } else {
-    //         header.classList.remove("small-header");
-    //     }
-    // };
-    window.onscroll = function() {scrollFunction()};
+    window.addEventListener('resize', adjustName);
+    window.addEventListener('resize', scrollFunction);
 
-    header.onclick = function() {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    };
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        window.addEventListener('scroll', throttle(scrollFunction,100));
+    }else{
+        window.addEventListener('scroll', scrollFunction);
+    }
+    
 });
 
 
-
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementsByClassName("header").style.fontSize = "30px";
-
-    } else {
-      document.getElementsByClassName("header").style.fontSize = "90px";
-    }
+function toggleMenu() {
+    var menu = document.querySelector('.navigation ul');
+    var menuIcon = document.querySelector('.hamburger-menu');
+    menu.classList.toggle('show');
+    menuIcon.classList.toggle('menu-toggled');
 }
